fc61907116285fdc0e6f06c25222af688d9fa647
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-03-16 17:19:44 -0300

    build: add utils(list-links) to index.js

---
+++ script/links.js
@@ -0,0 +1,27 @@
+export const links = [
+  {
+    name: 'Instagram',
+    url: 'https://www.instagram.com/tailane.isaias/',
+    color: 'red'
+  },
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/tailaneisaias',
+    color: 'blue'
+  },
+  {
+    name: 'Linkedin',
+    url: 'https://www.linkedin.com/in/tailaneisaias/',
+    color: 'blue'
+  },
+  {
+    name: 'Behance',
+    url: 'https://www.behance.net/tailaneisaias',
+    color: 'blue'
+  },
+  {
+    name: 'Email',
+    url: 'mailto:tailaneisaias08@gmail.com',
+    color: 'orange'
+  }
+]
