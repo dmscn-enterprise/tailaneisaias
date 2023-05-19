d042924cc4d0c85170bcd0d2590150283e2029ea
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-05-19 10:33:45 -0300

    feat(links): add link to profile-image

--- script/links.js
+++ script/links.js
@@ -1,27 +1,29 @@
+export const profileImage = "./assets/profile_image.jpg";
+
 export const links = [
   {
-    name: 'Instagram',
-    url: 'https://www.instagram.com/tailane.isaias/',
-    color: 'red'
+    name: "Instagram",
+    url: "https://www.instagram.com/tailane.isaias/",
+    color: "red",
   },
   {
-    name: 'Twitter',
-    url: 'https://twitter.com/tailaneisaias',
-    color: 'blue'
+    name: "Twitter",
+    url: "https://twitter.com/tailaneisaias",
+    color: "blue",
   },
   {
-    name: 'Linkedin',
-    url: 'https://www.linkedin.com/in/tailaneisaias/',
-    color: 'blue'
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/tailaneisaias/",
+    color: "blue",
   },
   {
-    name: 'Behance',
-    url: 'https://www.behance.net/tailaneisaias',
-    color: 'blue'
+    name: "Behance",
+    url: "https://www.behance.net/tailaneisaias",
+    color: "blue",
   },
   {
-    name: 'Email',
-    url: 'mailto:tailaneisaias08@gmail.com',
-    color: 'orange'
-  }
-]
+    name: "Email",
+    url: "mailto:tailaneisaias08@gmail.com",
+    color: "orange",
+  },
+];
