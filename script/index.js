9e98f942bd9b2168bc301dfdff41b643ea4ceafc
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-05-19 10:34:49 -0300

    feat(index): implement profileImage render

--- script/index.js
+++ script/index.js
@@ -1,15 +1,18 @@
-import { links } from "./links.js";
+import { profileImage, links } from "./links.js";
 
 function renderLinks(list) {
-  const DOM = document.getElementById('links');
+  const profileHTML = document.querySelector(".profile-image");
+  const links = document.getElementById("links");
+
+  profileHTML.setAttribute("src", profileImage);
 
   list.map((item) => {
-    DOM.innerHTML += `
+    links.innerHTML += `
       <a class="link ${item.color}" href="${item.url}">
         ${item.name}
       </a>
-    `
-  })
+    `;
+  });
 }
 
 renderLinks(links);
